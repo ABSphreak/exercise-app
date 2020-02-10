a12f9d0bf1a7ee8d0cd64edd7ce02f0fcfcdc971
ABSphreak <absphreak@outlook.com>
2020-02-10 21:48:14 +0530

    Adding store.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React, { Component, Fragment } from "react";
 import { Header, Footer } from "./Layouts";
 import Exercises from "./Exercises";
+import { muscles, exercises } from "../store";
 
 export default class App extends Component {
+  state = {
+    exercises
+  };
+
   render() {
     return (
       <Fragment>
--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,3 +1,17 @@
 import React from "react";
+import { Grid, Paper } from "@material-ui/core";
 
-export default props => <div>Hello from Exercises</div>;
+const styles = {
+  Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
+};
+
+export default props => (
+  <Grid container sm>
+    <Grid item sm>
+      <Paper style={styles.Paper}>Left</Paper>
+    </Grid>
+    <Grid item sm>
+      <Paper style={styles.Paper}>Right</Paper>
+    </Grid>
+  </Grid>
+);
